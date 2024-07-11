bcd55923a5248d800d13531963741def8dc261a3
Sandrah <comfortsandrah13@gmail.com>
2024-07-12 00:28:36 +0300

    Calculate Year of Birth

--- headphones/script.js
+++ headphones/script.js
@@ -1,25 +1,34 @@
 const name = "Sandrah";
-console.log(name,typeof name);
+console.log(name, typeof name);
 
 const age = 19;
-console.log(age,typeof age);
+console.log(age, typeof age);
 
 const isStudent = true;
-console.log(isStudent,typeof isStudent);
+console.log(isStudent, typeof isStudent);
 
 
 const address = {
     city: "Nairobi",
     country: "Kenya",
 }
-console.log(address,typeof address);
+console.log(address, typeof address);
 
 const hobbies = ["singing", "coding", "dancing", "reading"];
-console.log(hobbies,typeof hobbies);
+console.log(hobbies, typeof hobbies);
 
 
 let undefinedVariable;
-console.log(undefinedVariable,typeof undefinedVariable);
+console.log(undefinedVariable, typeof undefinedVariable);
 
 let nullVariable = null;
-console.log(nullVariable,typeof nullVariable);
+console.log(nullVariable, typeof nullVariable);
+
+
+const userName = prompt('Please enter your name:');
+const userAge = prompt('Please enter your age:');
+
+const year = new Date().getFullYear();
+const yearOfBirth = year - userAge;
+
+alert(`Your name is ${userName} and your year of birth is ${yearOfBirth}`);

63404a2cf9fa90517542443b21b4d96dd1ea7f91
Sandrah <comfortsandrah13@gmail.com>
2024-07-11 23:46:03 +0300

    learn js variables

---
+++ headphones/script.js
@@ -0,0 +1,25 @@
+const name = "Sandrah";
+console.log(name,typeof name);
+
+const age = 19;
+console.log(age,typeof age);
+
+const isStudent = true;
+console.log(isStudent,typeof isStudent);
+
+
+const address = {
+    city: "Nairobi",
+    country: "Kenya",
+}
+console.log(address,typeof address);
+
+const hobbies = ["singing", "coding", "dancing", "reading"];
+console.log(hobbies,typeof hobbies);
+
+
+let undefinedVariable;
+console.log(undefinedVariable,typeof undefinedVariable);
+
+let nullVariable = null;
+console.log(nullVariable,typeof nullVariable);
